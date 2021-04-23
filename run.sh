af0d877100ad73cc2ea165e4ad1a89444f92268f
abpabab <abpabab@gmail.com>
2021-04-22 22:35:15 -0700

    Tweak boilerplate CONFIG

--- run.sh
+++
@@ -1,5 +0,0 @@
-if [ -z $FLASK_CONFIG ]; then
-    export FLASK_CONFIG="development"
-fi
-
-/usr/bin/env python3 simple_app.py
--- simple_app.py
+++ simple_app.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 # @Author: ubuntu
 # @Date:   2021-04-22 08:31:53
-# @Last Modified by:   ubuntu
-# @Last Modified time: 2021-04-22 21:28:39
+# @Last Modified by:   abpabab
+# @Last Modified time: 2021-04-22 22:34:05
 
 
 from os import environ
@@ -10,14 +10,17 @@ from sys import exit
 from config import config_dict
 from app import create_app
 
-get_config_mode = environ.get('FLASK_CONFIG', 'development')
+get_config_mode = environ.get('FLASK_ENV', 'development')
 
 try:
     config_mode = config_dict[get_config_mode]
 except KeyError:
-    exit('Error: Invalid FLASK_CONFIG environment variable entry.')
+    exit('Error: Invalid FLASK_ENV environment variable entry.')
 
 app = create_app(config_mode)
 
 if get_config_mode == 'development':
-    app.run(host="127.0.0.1, port=5000, debug=True)
+    app.run(host="127.0.0.1", port=5000)
+
+if get_config_mode == 'production':
+    app.run(host="127.0.0.1", port=5000)
